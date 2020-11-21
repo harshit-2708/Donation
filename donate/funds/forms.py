7278a62d0c9416a6739c63185f3e038466cabc3a
harshit jain <harshuharshit27@gmail.com>
2020-11-21 19:00:58 +0530

    initial commit

---
+++ donate/funds/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class DonateForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    number = forms.IntegerField()
+    amount = forms.IntegerField()
